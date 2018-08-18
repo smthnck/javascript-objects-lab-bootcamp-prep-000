90c6f0a0ea2ae6049416071e980ff403a866cedc
Nick Smith <smthnck@gmail.com>
2018-08-18 18:32:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {}
+var updateObjectWithKeyAndValue = function(obj, key, value){
+  return Object.assign({},obj,{ [key]:value })
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  object.key=value
+  return object
+}
